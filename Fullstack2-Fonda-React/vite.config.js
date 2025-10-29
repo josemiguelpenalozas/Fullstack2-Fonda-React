36dc338d7da408f16e6d1d2c24ad57fca37051e0
Benjamin Arellano <be.arellanor@duocuc.cl>
2025-10-28 22:48:12 -0300

    Pagina link

--- Fullstack2-Fonda-React/vite.config.js
+++ Fullstack2-Fonda-React/vite.config.js
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets',
+          dest: 'src'        
+        }
+      ]
+    }),
+  ],
+  base: './',
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js"
-  }
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
 })
